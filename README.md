f9e82fb9ff73f39791152f8a0d131854705931b2
Jude <45480093+JudeTe@users.noreply.github.com>
2023-05-12 13:52:50 +0800

    Remove Multi-processing design

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PttImageCrawler
 
 ## Overview
-PttImageCrawler is a web crawling tool that crawls images from PTT (a bulletin board system in Taiwan). You can specify the board, pages, path, and even the number of threads or processes you want to use for crawling.
+PttImageCrawler is a web crawling tool that crawls images from PTT (a bulletin board system in Taiwan). You can specify the board, pages, path, and even the number of threads you want to use for crawling.
 
 ## Installation
 Clone this repository:
@@ -16,7 +16,7 @@ python -m pip install -r requirements.txt
 
 ## Usage
 ```
-crawler.py [-h] [-b nba] [--pages 10] [--path C://] [--dir nba] [-t 10] [-p 4]
+crawler.py [-h] [-b nba] [--pages 10] [--path C://] [--dir nba] [-t 10]
 ```
 
 optional arguments:
@@ -25,8 +25,7 @@ optional arguments:
 *  --pages `10`  specify how many pages you want to download in the given board (default: 1)  
 *  --path `C://`  specify the path for storing the file (default: './')  
 *  -d `nba` --dir `nba` specify the directory name for storing the file (default: '{board name}')  
-*  -t `10` thread `10` specify how many threads to use for running the program. (default: 0)  
-*  --process `4` specify how many processes to use for running the program. (default: 0)  
+*  -t `8` thread `8` specify how many threads to use for running the program. (default: numbers of your core)  
 
 
 Custom arguments example:
@@ -34,7 +33,7 @@ Custom arguments example:
 python crawler.py -b nba --pages 10 --path ./ --dir nba -t 10
 ```
 
-P.S. If the number of processes and threads is not specified, the default is to use the number of cores in the current system as the number of processes to be used.
+P.S. If the number of threads is not specified, the default is to use the number of cores in the current system as the number of thread to be used.
 
 ## License
 This project is licensed under the MIT License - see the LICENSE.txt file for details.
--- crawler.py
+++ crawler.py
@@ -1,13 +1,13 @@
 # Encoding: utf-8
 # Author: JudeTe
 # Repo: https://github.com/JudeTe/PttImageCrawler
-# Date: 2023-05-10
+# Date: 2023-05-12
 # Python version: 3.9
 
 
 """Crawl any board from PTT and download all images from the articles.
-Usage: python crawler.py -b <board name> -p <pages> -d <directory name> -t <threads> -p <processes>
-Example: python crawler.py -b beauty -p 1 -d beauty -t 4 -p 4
+Usage: python crawler.py -b <board name> --pages <pages> --path <path> -d <directory name> -t <threads>
+Example: python crawler.py -b nba --pages 10 --path ./ --dir nba -t 10
 """
 
 
@@ -15,9 +15,6 @@ import os
 import time
 import queue
 import threading
-import multiprocessing
-from multiprocessing import Process, Pool
-from multiprocessing import Queue as MQ
 import argparse
 import requests
 from bs4 import BeautifulSoup
@@ -27,39 +24,46 @@ numbers_of_core = os.cpu_count()
 parser = argparse.ArgumentParser(description='Ptt Crawler')
 parser.add_argument('--board', '-b', type=str, default='beauty', 
                     help='specify the board you want to download (default: "beauty")')
-parser.add_argument('--pages', type=int, default=1, 
+parser.add_argument('--pages', '-P', type=int, default=1, 
                     help='specify how many pages you want to download in the given board \
                     (default: 1)')
-parser.add_argument('--path', type=str, default='', 
+parser.add_argument('--path', '-p', type=str, default='', 
                     help='specify the path for storing the file (default: "./")')
 parser.add_argument('--dir', '-d', type=str, default='', 
                     help='specify the directory name for storing the file \
                     (default: "{board name}")')
-parser.add_argument('--thread', '-t', type=int, default=0, 
+parser.add_argument('--thread', '-t', type=int, default=numbers_of_core, 
                     help='specify how many threads to use for running the program. \
-                    (default: 0)')
-parser.add_argument('--process', type=int, default=0,
-                    help='specify how many processes to use for running the program. \
-                    (default: 0)')
+                    (default: numbers of your core)')
+
 args = parser.parse_args()
 board = args.board
 pages = args.pages
 path = args.path if args.path else os.path.dirname(os.path.abspath(__file__))
 directory_name = args.dir if args.dir else board
-thread_num = args.thread
-process_num = args.process
-
-if not thread_num and not process_num:
-    process_num = numbers_of_core
-elif thread_num and process_num:
-    thread_num = None
-    process_num = numbers_of_core
 directory_path = f"{path}/{directory_name}/"
 if not os.path.exists(directory_path):
     os.mkdir(directory_path)
+thread_num = args.thread
+
 BASEPAGE = 0
-download_count = 0
 BOARD_PREFIX = f"https://www.ptt.cc/bbs/{board}"
+download_count = 0
+
+class Worker(threading.Thread):
+    """Worker for scraping"""
+    def __init__(self, queue, num):
+        # super().__init__()
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.num = num
+
+    def run(self):
+        while self.queue.qsize() > 0:
+            url = self.queue.get()
+            print(f"Worker {self.num}: {url}")
+            img_crawler(url)
+
 
 def article_crawler(q: queue) -> None:
     """Scrape articles from given pages"""
@@ -75,6 +79,8 @@ def article_crawler(q: queue) -> None:
             except Exception as err_:
                 print(f"Error: {err_}")
                 continue
+    return q
+
 
 def img_crawler(article_suffix: str) -> None:
     """Scrape img from given article"""
@@ -97,64 +103,18 @@ def img_crawler(article_suffix: str) -> None:
             print(f"Error: {err_}")
             continue
 
-def crawl_process(queue: queue, workers=None) -> None:
-    """Non blocking get queue"""
-    for i in range(10):
-        t = Worker(queue, i)
-        t.start()
-        workers.append(t)
-    # while queue.qsize() > 0:
-    #     url = queue.get_nowait()
-    #     img_crawler(url)
-
-class Worker(threading.Thread):
-    """Worker for scraping"""
-    def __init__(self, queue, num):
-        # super().__init__()
-        threading.Thread.__init__(self)
-        self.queue = queue
-        self.num = num
-
-    def run(self):
-        while self.queue.qsize() > 0:
-            url = self.queue.get()
-            print(f"Worker {self.num}: {url}")
-            img_crawler(url)
 
 if __name__ == "__main__":
     start_time = time.time()
-    crawler_queue = multiprocessing.Queue()
-    article_crawler(crawler_queue)
+    crawler_queue = article_crawler(queue.Queue())
     print(f"Total articles: {crawler_queue.qsize()}")
     workers = []
-    worker_nums = thread_num if thread_num else process_num
-    for i in range(worker_nums):
-        if thread_num:
-            t = Worker(crawler_queue, i)
-            t.start()
-            workers.append(t)
-        elif process_num:
-            p = Process(target=crawl_process, args=(crawler_queue, workers))
-            p.start()
-            workers.append(p)
+    for i in range(thread_num):
+        t = Worker(crawler_queue, i)
+        t.start()
+        workers.append(t)
     for worker in workers:
         worker.join()
-
-    # with Pool(processes=8) as pool:
-    #     responses = pool.apply_async(crawl_process, args=(crawler_queue, ))
-
-    # pool = Pool(worker_nums)
-    # pool_outputs = pool.map(crawl_process, (crawler_queue, ))
-
-    # pool = Pool(worker_nums)
-    # pool_outputs = pool.map_async(crawl_process, (crawler_queue, ))
-    # pool.close()
-    # pool.join()
-
     end_time = time.time()
     print(f"Time takes: {end_time - start_time} seconds.")
     print(f"Download {download_count} files.")
-
-
-
-
